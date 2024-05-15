8b35c1d570c9426d5c125c2069afdfe58156302a
Sivabalan10 <135865311+Sivabalan10@users.noreply.github.com>
2024-05-15 22:42:37 +0530

    framework set uped

--- webpage/app.py
+++ webpage/app.py
@@ -1,1 +1,54 @@
-# Flask code
+from flask import Flask,render_template,redirect,request,jsonify,url_for
+
+app = Flask(__name__)
+
+# home page 
+@app.route("/")
+def homepage():
+    return "Login page"
+    
+# Login call
+@app.route("/login")
+def login():
+    return "home page"
+# Logout call
+@app.route("/logout")
+def logout():
+    return redirect('/')
+
+# Sign_up page
+@app.route("/sign_up")
+def sign_up():
+    return "sign_up page"
+
+# Storing account
+@app.route("/sign_up/account_added")
+def account_added():
+    return redirect('/')
+
+# group_joined
+@app.route("/group_joined")
+def group_joined():
+    return "list the group"
+
+# group details
+@app.route("/groups_joined/group_details")
+def group_details():
+    return "dynammic info about group"
+
+# other members
+@app.route("/groups_joined/group_details/other_members")
+def other_members():
+    return "other members page"
+
+# join group
+@app.route("/join_group")
+def join_group():
+    return "listing groups using search with join icon"
+
+@app.route("/profile")
+def profile():
+    return "profile with edit icon in terms of form (easy approach)"
+
+if  __name__ == "__main__":
+    app.run(debug=True)
